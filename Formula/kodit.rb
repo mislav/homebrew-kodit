6f7dbe8844f5b87167d7b872980cd98a4a3afebd
Mislav Marohnić <hi@mislav.net>
2025-05-30 16:23:56 +0200

    Upgrade kodit to 0.1.13

--- Formula/kodit.rb
+++ Formula/kodit.rb
@@ -3,8 +3,8 @@ class Kodit < Formula
   
     desc "Kodit is an MCP server that indexes your private codebases"
     homepage "https://docs.helixml.tech/kodit/"
-    url "https://pypi.io/packages/source/k/kodit/kodit-0.1.12.tar.gz"
-    sha256 "527b4cd9c006eb5c54af553bdcecfe07fcd957bcdb01a2a6dab21a353dc837ef"
+    url "https://pypi.io/packages/source/k/kodit/kodit-0.1.13.tar.gz"
+    sha256 "475ee920c3bde32370bb4e8c4185d04beb6e47e6799427e36d21b388d694d8ec"
     license "Apache-2.0"
   
     depends_on "python@3.12"
